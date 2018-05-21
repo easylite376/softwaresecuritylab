d100fa54dd9c88e08897e091edad458e1fc17036
Martin Staehr <martin@mstaehr.net>
2018-05-22 00:00:03 +0200

    adding searchFiles function skeleton

--- lab01/tool.py
+++ lab01/tool.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python3
 
-import argparse
+import argparse,urllib
+
+SUBFOLDER = ['backup']
+
+def searchFiles(url,filenameList):
+    for filename in filenameList:
+        #requestUrl("/".join([url,filename])
+        for dir in SUBFOLDER:
+            seq = "/".join([url,dir,filename])
+            #requestUrl(seq)
+            print(seq)
+
+#def requestUrl(url):
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument("url",help="Url you want to test")
@@ -10,11 +23,12 @@ args = parser.parse_args()
 if args.file:
     print("i turned on",args.file)
     f = open("test.txt","r")
-    for fileName in f.read().split():
-        print(fileName)
+    filenameList = f.read().split()
     f.close()
 if args.bruteforce:
     print("brutefoce turned on", args.bruteforce)
+
+searchFiles(args.url,filenameList)
 
 if args.url:
     print("url=",args.url)
