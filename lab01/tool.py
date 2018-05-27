d1870604d7579b3c518181e02cc04558fca020bf
Martin Staehr <martin@mstaehr.net>
2018-05-27 17:49:29 +0200

    adding bruteforce functionallity
    TODO: fix itertools combination

--- lab01/tool.py
+++ lab01/tool.py
@@ -1,8 +1,9 @@
 #!/usr/local/bin/python3
 
-import argparse
+import argparse,string
 from urllib.request import urlopen
 from urllib.error import HTTPError
+from itertools import combinations
 
 SUBFOLDER = ['backup']
 
@@ -14,8 +15,8 @@ def searchFiles(url,filenameList):
         urlWithoutDir = "/".join([url,filename])
         if (isPresentOnServer(urlWithoutDir)):
             presentFiles.append(urlWithoutDir)
-        for dir in SUBFOLDER:
-            urlWithDir = "/".join([url,dir,filename])
+        for directory in SUBFOLDER:
+            urlWithDir = "/".join([url,directory,filename])
             if (isPresentOnServer(urlWithDir)):
                 presentFiles.append(urlWithDir)
     print(presentFiles)
@@ -26,10 +27,20 @@ def isPresentOnServer(url):
     except HTTPError:
         return False
 
+def createAllPossibleStrings(combinationLength):
+    alphabet = list(string.ascii_lowercase)
+    combinationList = list()
+    extensions = [".cf",".txt",".conf",".bak",".conf.bak",".sql",".data"]
+    for combinationTuple in combinations(alphabet,combinationLength):
+        for extension in extensions:
+            combinationList.append("".join(str(tupleElement) for tupleElement in combinationTuple) + extension)    
+    return combinationList
+
 parser = argparse.ArgumentParser()
 parser.add_argument("url",help="Url you want to test")
 parser.add_argument("-i", dest="file" ,help="Wordlist")
-parser.add_argument("-bruteforce",dest='bruteforce', action='store_true', help="Bruteforces the potential filenames")
+parser.add_argument("-bruteforce",dest='bruteforce', action='store_true',
+                    help="Bruteforces the potential filenames")
 args = parser.parse_args()
 if args.file:
     print("i turned on",args.file)
@@ -38,8 +49,11 @@ if args.file:
     f.close()
 if args.bruteforce:
     print("brutefoce turned on", args.bruteforce)
-
-searchFiles(args.url,filenameList)
+    filenameList = createAllPossibleStrings(5)
+    filenameList.append("backup.txt")
+    filenameList.append("config.txt")
+if args.file or args.bruteforce:
+    searchFiles(args.url,filenameList)
 
 if args.url:
-    print("url=",args.url)
+    print("url =",args.url)
