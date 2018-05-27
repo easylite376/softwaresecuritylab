0831e80aac13277ef94eb36af3673d289d3a37e7
Martin Staehr <martin@mstaehr.net>
2018-05-27 16:21:49 +0200

    adding check if file exist on the server

--- lab01/tool.py
+++ lab01/tool.py
@@ -1,19 +1,30 @@
-#!/usr/bin/python3
+#!/usr/local/bin/python3
 
-import argparse,urllib
+import argparse
+from urllib.request import urlopen
+from urllib.error import HTTPError
 
 SUBFOLDER = ['backup']
 
 def searchFiles(url,filenameList):
+    presentFiles = list()
+    if (url[-1:] == "/"):
+        url = url[:-1]
     for filename in filenameList:
-        #requestUrl("/".join([url,filename])
+        urlWithoutDir = "/".join([url,filename])
+        if (isPresentOnServer(urlWithoutDir)):
+            presentFiles.append(urlWithoutDir)
         for dir in SUBFOLDER:
-            seq = "/".join([url,dir,filename])
-            #requestUrl(seq)
-            print(seq)
+            urlWithDir = "/".join([url,dir,filename])
+            if (isPresentOnServer(urlWithDir)):
+                presentFiles.append(urlWithDir)
+    print(presentFiles)
 
-#def requestUrl(url):
-
+def isPresentOnServer(url):
+    try:
+        return urlopen(url).getcode() == 200
+    except HTTPError:
+        return False
 
 parser = argparse.ArgumentParser()
 parser.add_argument("url",help="Url you want to test")
