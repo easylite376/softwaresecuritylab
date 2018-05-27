ffe631019ff641d8eef134f46826019265ed9d05
Martin Staehr <martin@mstaehr.net>
2018-05-27 20:14:57 +0200

    adding progress, report to terminal

--- lab01/tool.py
+++ lab01/tool.py
@@ -8,24 +8,42 @@ from itertools import combinations
 SUBFOLDER = ['backup']
 
 def searchFiles(url,filenameList):
-    presentFiles = list()
+    availUrls = list()
+    filenameListLength = len(filenameList)
+    lastProgress = 0
+    counter = 0
+    print("Progress in %: ")
     if (url[-1:] == "/"):
         url = url[:-1]
     for filename in filenameList:
+        counter += 1
+        lastProgress = displayProgress(int(counter*100/filenameListLength),lastProgress)
         urlWithoutDir = "/".join([url,filename])
         if (isPresentOnServer(urlWithoutDir)):
-            presentFiles.append(urlWithoutDir)
+            availUrls.append(urlWithoutDir)
         for directory in SUBFOLDER:
             urlWithDir = "/".join([url,directory,filename])
             if (isPresentOnServer(urlWithDir)):
-                presentFiles.append(urlWithDir)
-    print(presentFiles)
+                availUrls.append(urlWithDir)
+    print()
+    createReport(availUrls)
 
 def isPresentOnServer(url):
     try:
         return urlopen(url).getcode() == 200
     except HTTPError:
         return False
+
+def createReport(reachableUrls):
+    print("-------------------------------------------------")
+    if (len(reachableUrls) != 0):
+        print("The following urls are reachable on the webserver")
+        print("-------------------------------------------------")
+        for url in reachableUrls:
+            print(url)
+    else:
+        print("Your webserver is save!")
+
 
 def createAllPossibleStrings(combinationLength):
     alphabet = list(string.ascii_lowercase)
@@ -35,6 +53,12 @@ def createAllPossibleStrings(combinationLength):
         for extension in extensions:
             combinationList.append("".join(str(tupleElement) for tupleElement in combinationTuple) + extension)    
     return combinationList
+
+def displayProgress(actualProgress,lastProgress):
+    if actualProgress != lastProgress:
+        lastProgress = actualProgress
+        print(str(lastProgress) + " ",end="",flush=True)
+    return lastProgress
 
 parser = argparse.ArgumentParser()
 parser.add_argument("url",help="Url you want to test")
@@ -48,12 +72,10 @@ if args.file:
     filenameList = f.read().split()
     f.close()
 if args.bruteforce:
-    print("brutefoce turned on", args.bruteforce)
-    filenameList = createAllPossibleStrings(5)
-    filenameList.append("backup.txt")
+    print("bruteforce turned on", args.bruteforce)
+    filenameList = createAllPossibleStrings(2)
+    # TODO Remove testfiles
+    filenameList.append("password.txt")
     filenameList.append("config.txt")
 if args.file or args.bruteforce:
     searchFiles(args.url,filenameList)
-
-if args.url:
-    print("url =",args.url)
