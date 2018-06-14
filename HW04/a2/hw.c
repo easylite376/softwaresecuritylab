c5b94bb0fed265f5d4a2a5e54fe1cf400b5f88c1
Max Schrötter <max.schroetter@gmail.com>
2018-06-14 21:25:34 +0200

    Added COmments for Ashouri

--- HW04/a2/hw.c
+++ HW04/a2/hw.c
@@ -10,24 +10,24 @@ void vuln_copy (char *attack_source)
     strcpy(dest, attack_source);
     if(canary!='1')
         exit(0);
-    asm volatile ( "movq %%rbp, %%rax \n"
-            "addq $8, %%rax \n"
-            "movq (%%rax), %%rax \n "
-            "cmpq $0x12345678, (%%rax) \n"
-            "je  .LMAX\n "
-            "movl    $1, %%edi \n"
-            "call    exit@PLT \n"
-            ".LMAX: \n"
-            "addq $8, 8(%%rbp)"
+    asm volatile ( "movq %%rbp, %%rax \n" //Get the stack base pointer into general purpose register
+            "addq $8, %%rax \n"  //increase registe to return address
+            "movq (%%rax), %%rax \n " // dereference return address
+            "cmpq $0x12345678, (%%rax) \n" //compare label at the return address with static saved label to ensure Programm flow
+            "je .LMAX\n " // Jump in success to Label MAX
+            "movl $1, %%edi \n" // Set exit value to one in case of manipulation
+            "call exit@PLT \n" // exit with EXIT_FAILURE
+            ".LMAX: \n" // Label MAX
+            "addq $8, 8(%%rbp)" //Add 8 to the return address to bypass label and execute next instruction in main.
             :
             :
-            :"rax");
+            :"rax"); //cobbler register (Register used in the assembly code)
 }
 int main (int argc, char **argv)
 {
 // call the vulnerable function
     vuln_copy(argv[1]);
-    asm volatile ( "; .quad 0x12345678 \n");
+    asm volatile ( "; .quad 0x12345678 \n"); // The label used for the check
 
 }
 void hack_me ()
