26aee46c89a337d66860c4f2ecaa410751c86d95
Martin Stähr <martin@mstaehr.net>
2018-06-20 19:57:52 +0200

    add bash script for sign and align

--- lab03/test_orig/dist/signAndAlign.sh
+++ lab03/test_orig/dist/signAndAlign.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore test.keystore test.apk test_key
+jarsigner -storepass test1234 -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore test.keystore test.apk test_key
 zipalign -f -v 4 test.apk test_aligned.apk
+
