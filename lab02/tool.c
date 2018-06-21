386b6df1ef779ce3bd0fe65b02e09e0e972b4c23
Max Schrötter <max.schroetter@gmail.com>
2018-06-21 23:22:53 +0200

    added argument parsing and random calls

--- lab02/tool.c
+++ lab02/tool.c
@@ -1,15 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <string.h>
 #include <unistd.h> /* for fork */
 #include <sys/types.h> /* for pid_t */
 #include <sys/wait.h> /* for wait */
 #include "randm.h"
+#include <getopt.h>
 
-int main()
+int main(int argc,char** argv)
 {
+    struct stat sb;
+    if (argc!=3){
+        printf("This tool requires 2 arguments \n tool.out PROGRAM OVERFLOW|FORMATSTRING \n");
+        exit(1);
+    }
+    if(! ( strncmp(argv[2],"OVERFLOW",8)  || strncmp(argv[2],"FORMATSTRING",12))){
+        printf("This tool requires 2 arguments \n tool.out PROGRAM OVERFLOW|FORMATSTRING \n");
+        exit(1);
+    }
+    if( access( argv[1], F_OK ) < 0 ) {
+        printf("This tool requires 2 arguments \n tool.out PROGRAM OVERFLOW|FORMATSTRING \n");
+        exit(1);
+    }
+    int ret = stat(argv[1], &sb);
+    if (!(ret == 0 && (sb.st_mode & S_IXUSR))) {
+        printf("This tool requires 2 arguments \n tool.out PROGRAM OVERFLOW|FORMATSTRING \n");
+        exit(1);
+    }
+
     char arr[16];
     char arr2[16];
-    rand_str_dc(arr,15);
+    rand_str_dc(arr,16);
     rand_str_ndc(arr2,16);
 
     printf("%s \n",arr);
