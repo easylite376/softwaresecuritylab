3d0e110da0ca546fe1d6004147487ea7dc81026a
Max Schrötter <max.schroetter@gmail.com>
2018-06-28 12:15:43 +0200

    changed buffer size

--- lab02/vuln.c
+++ lab02/vuln.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 int main (int argc, char ** argv)
 {
-    char temp[2048];       // string to hold large temp string
+    char temp[212];       // string to hold large temp string
     strcpy(temp, argv[1]);   // take argv1 input and jam into temp
     printf(temp);
 }
